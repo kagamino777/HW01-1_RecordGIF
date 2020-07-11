e6744afc612abb0f03ea38f1dcbc59b3cab4df13
jabachia <kagamino777@gmail.com>
2020-07-11 17:15:51 +0800

    0707_work in class(ALL)

---
+++ First0707/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First0707
+//
+//  Created by JaBa CHIA on 2020/7/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ First0707Tests/First0707Tests.swift
@@ -0,0 +1,33 @@
+//
+//  First0707Tests.swift
+//  First0707Tests
+//
+//  Created by JaBa CHIA on 2020/7/7.
+//
+
+import XCTest
+@testable import First0707
+
+class First0707Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
